79d3cdc7101c70cd979342fcb0374dcaa8ffd6ec
Shuhao <johnmave126@gmail.com>
2017-09-17 18:09:09 -0400

    Add AO Integrator

---
+++ src/ao.cpp
@@ -0,0 +1,32 @@
+#include <nori/integrator.h>
+#include <nori/scene.h>
+#include <nori/sampler.h>
+#include <nori/warp.h>
+
+NORI_NAMESPACE_BEGIN
+
+class AOIntegrator : public Integrator {
+public:
+	AOIntegrator(const PropertyList &props) {}
+
+	Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &ray) const {
+		/* Find the surface that is visible in the requested direction */
+		Intersection its;
+		if (!scene->rayIntersect(ray, its))
+			return Color3f(0.0f);
+		uint32_t acc = 0;
+		for (uint32_t i = 0; i < sampler->getSampleCount(); ++i) {
+			Vector3f w = Warp::squareToCosineHemisphere(sampler->next2D());
+			Vector3f shadow = its.shFrame.toWorld(w);
+			acc += !scene->rayIntersect(Ray3f(its.p, shadow));
+		}
+		return Color3f(1.0f * acc / sampler->getSampleCount());
+	}
+
+	std::string toString() const {
+		return "AmbientOcclusionIntegrator[]";
+	}
+};
+
+NORI_REGISTER_CLASS(AOIntegrator, "ao");
+NORI_NAMESPACE_END
